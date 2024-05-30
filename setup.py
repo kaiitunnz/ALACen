5ae9801da0ab257cd4358f822fd22b7bdf2fffda
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-05-30 14:17:07 +0900

    Fix installation issues

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import subprocess
 from setuptools import setup  # type:ignore
 
 from src.alacen import __version__
 
 with open("requirements.txt", "r") as f:
     install_requires = [line for line in f if not line.lstrip().startswith("#")]
+
+if subprocess.call("pip install numpy", shell=True):
+    raise Exception("Failed to install numpy")
 
 setup(
     name="alacen",
