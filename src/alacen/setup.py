f6fc17615b4f46aaa7c6491ee552336ceaeb02a1
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-05-30 14:01:10 +0900

    Fix bugs in the installation script

--- src/alacen/setup.py
+++ src/alacen/setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def setup():
-    if subprocess.call("conda install -y conda-forge::ffmpeg"):
+    if subprocess.call("conda install -y conda-forge::ffmpeg", shell=True):
         raise Exception("Failed to install ffmpeg")
 
     # Install dependencies
@@ -20,25 +20,29 @@ def setup():
         "huggingface_hub==0.22.2",
     ]
     for d in dependencies:
-        if subprocess.call(f"pip install {d}"):
+        if subprocess.call(f"pip install {d}", shell=True):
             raise Exception(f"Failed to install {d}")
 
     # Install MFA for getting forced-alignment, this could take a few minutes
     if subprocess.call(
-        "conda install -y -c conda-forge montreal-forced-aligner=2.2.17 openfst=1.8.2 kaldi=5.5.1068"
+        "conda install -y -c conda-forge montreal-forced-aligner=2.2.17 openfst=1.8.2 kaldi=5.5.1068",
+        shell=True,
     ):
         raise Exception("Failed to install MFA")
     # Install MFA english dictionary and model
-    if subprocess.call("mfa model download dictionary english_us_arpa"):
+    if subprocess.call("mfa model download dictionary english_us_arpa", shell=True):
         raise Exception(f"Failed to install MFA dictionary")
-    if subprocess.call("mfa model download acoustic english_us_arpa"):
+    if subprocess.call("mfa model download acoustic english_us_arpa", shell=True):
         raise Exception(f"Failed to install MFA acoustic")
 
     diff2lip_dir = Path(__file__).parent / "lipsync" / "diff2lip"
     # Install Diff2Lip dependencies
-    if subprocess.call(f"pip install -e {diff2lip_dir / 'guided-diffusion'}"):
+    if subprocess.call(
+        f"pip install -e {diff2lip_dir / 'guided-diffusion'}", shell=True
+    ):
         raise Exception("Failed to install guided-diffusion")
     if subprocess.call(
-        f"pip install gdown && gdown --folder 1UMiHAhVf5M_CKzjVQFC5jkz-IXAAnFo5 -O {diff2lip_dir / 'Diff2Lip_checkpoints'}"
+        f"pip install gdown && gdown --folder 1UMiHAhVf5M_CKzjVQFC5jkz-IXAAnFo5 -O {diff2lip_dir / 'Diff2Lip_checkpoints'}",
+        shell=True,
     ):
         raise Exception("Failed to download Diff2Lip checkpoints")
