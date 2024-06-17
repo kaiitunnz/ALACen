3c6fb5d1cdd5919719cea6b3f9c1fd0076ee5498
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-06-17 18:26:52 +0900

    Add CLI args

--- src/alacen/__main__.py
+++ src/alacen/__main__.py
@@ -6,7 +6,31 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--video", type=Path, help="Path to the video file")
     parser.add_argument(
+        "-o", "--output", type=Path, help="Output directory", default=Path("output")
+    )
+    parser.add_argument(
         "-v", "--verbose", action="store_true", help="Verbose mode", default=False
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        choices=["auto", "semi"],
+        help="ALACen's mode of operation",
+        default="auto",
+    )
+    parser.add_argument(
+        "-np",
+        "--num-paraphrases",
+        type=int,
+        help="Number of candidate paraphrases",
+        default=5,
+    )
+    parser.add_argument(
+        "-ns",
+        "--num-speeches",
+        type=int,
+        help="Number of candidate speeches",
+        default=5,
     )
     return parser.parse_args()
 
@@ -31,16 +55,18 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 asr = Whisper()
 paraphrase = PegasusAlacen()
 tts = VoiceCraftTTS(model_name="330M_TTSEnhanced")
+voicecraft_args = VoiceCraftArgs.constructor(num_samples=args.num_speeches)
 lipsync = Diff2Lip(Diff2LipArgs())
 
 alacen = ALACen(asr, paraphrase, tts, lipsync)
 
 alacen.run(
     args.video,
-    "output",
-    VoiceCraftArgs,
-    num_paraphrases=5,
+    args.output,
+    voicecraft_args,
+    num_paraphrases=args.num_paraphrases,
     device=device,
+    mode=args.mode,
     verbose=args.verbose,
     clean_up=True,
 )
